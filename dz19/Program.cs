3427b26db0c940e4d21ab0c24730ff35f26609c9
Vika <vikka-94@mail.ru>
2022-12-19 23:29:58 +0300

    dz

---
+++ dz19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine ("Введите пятизначное значное число: ");
+string number = Console.ReadLine();
+int size = number.Length;
+if (size == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " Палиндром :)");
+    }
+    else
+    {
+        Console.WriteLine(number + " Не являетс палиндромом :(");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели НЕ 5-и значное число");
+}
---
+++ dz21/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек
+//и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите х1: ");
+int x1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите y1: ");
+int y1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите c1: ");
+int c1 = int.Parse (Console.ReadLine()!);
+
+Console.WriteLine("Введите x2: ");
+int x2 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите y3: ");
+int y2 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите z3: ");
+int c2 = int.Parse (Console.ReadLine()!);
+double distance = 0;
+
+distance = Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(c2-c1, 2)!);
+Console.WriteLine(Math.Round(distance,2)!);
